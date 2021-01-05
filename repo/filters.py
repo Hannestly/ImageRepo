7403d7d28098977619f557b1543c17b3ab6336ce
Hannestly <dooley994@gmail.com>
2021-01-04 19:10:12 -0500

    search multiple fields option

--- repo/filters.py
+++ repo/filters.py
@@ -1,8 +1,9 @@
 import django_filters
 from .models import *
 from django_filters import DateFilter
+from django.db.models import Q
 
-class ImageFilter(django_filters.FilterSet):
+class DetailFilter(django_filters.FilterSet):
     # start_date = DateFilter(field_name="date_created",lookup_expr=)
     title_filter = django_filters.CharFilter(field_name='title',lookup_expr='icontains')
     tag_filter = django_filters.CharFilter(field_name='tag', lookup_expr='icontains')
@@ -12,4 +13,13 @@ class ImageFilter(django_filters.FilterSet):
     class Meta:
         model = Image
         fields=[]
-        
+
+class CombinedFilter(django_filters.FilterSet):
+    combine_filter = django_filters.CharFilter(method='custom_combined_filter')
+
+    class Meta:
+        model = Image
+        fields=[]
+
+    def custom_combined_filter(self,queryset, name,value):
+        return Image.objects.filter(Q(title__icontains=value) | Q(tag__icontains=value) | Q(description__icontains=value))
